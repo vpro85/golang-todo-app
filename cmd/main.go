592e7dfbfc11b4bc84226610a23c4c07a538947d
Vadim Prokoshin <vpro85@gmail.com>
2022-02-13 20:25:21 +0200

    initial

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	todo "todo-app"
+)
+
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
