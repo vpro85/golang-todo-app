ba6950f8b7039680337b84d488094dac42fe08c9
Vadim Prokoshin <vpro85@gmail.com>
2022-02-13 21:10:24 +0200

    add repository and service

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,14 @@ import (
 	"log"
 	todo "todo-app"
 	handler "todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
