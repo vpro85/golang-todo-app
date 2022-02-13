3007b30feecd8ec28d99a38f5fef780617b7d80c
Vadim Prokoshin <vpro85@gmail.com>
2022-02-13 20:56:49 +0200

    add endpoints with empty handlers

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	todo "todo-app"
+	handler "todo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
--- server.go
+++ server.go
@@ -10,9 +10,10 @@ type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Run(port string) error {
+func (s *Server) Run(port string, handler http.Handler) error {
 	s.httpServer = &http.Server{
 		Addr:           ":" + port,
+		Handler:        handler,
 		TLSConfig:      nil,
 		MaxHeaderBytes: 1 << 20,
 		ReadTimeout:    10 * time.Second,
