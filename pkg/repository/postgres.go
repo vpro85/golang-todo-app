a82ff13cfb23e4e394ebb157622bbc789e4cf851
Vadim Prokoshin <vpro85@gmail.com>
2022-02-14 00:43:59 +0200

    add sign-up handler

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -5,6 +5,14 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+const (
+	usersTable      = "users"
+	todoListTable   = "todo_lists"
+	usersListsTable = "users_lists"
+	todoItemsTable  = "todo_items"
+	listsItemsTable = "lists_items"
+)
+
 type Config struct {
 	Host     string
 	Port     string
